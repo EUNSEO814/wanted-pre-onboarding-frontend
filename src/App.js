83fccad41cbb4a104cdc3b8ad7d3a8c67c45ec8a
EUNSEO814 <eunseo814@gmail.com>
2023-08-09 20:46:27 +0900

    ➕ react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Todo from "./pages/Todo";
+
 function App() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/todo" element={<Todo />} />
+    </Routes>
   );
 }
 
