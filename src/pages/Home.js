36c3da6a3dc5cd98fab1449eea797d0b6694d587
EUNSEO814 <eunseo814@gmail.com>
2023-08-10 21:07:12 +0900

    ✨ Add Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const LinkArea = styled.div`
+  background-color: inherit;
+`;
+
+const LinkSt = styled(Link)`
+  background-color: inherit;
+`;
 const Home = () => {
   return (
     <div>
       <h1>홈</h1>
+      <LinkArea>
+        <LinkSt to="/signup">회원가입</LinkSt>
+        <LinkSt to="/signin">로그인</LinkSt>
+        <LinkSt to="/todo">Todo List</LinkSt>
+      </LinkArea>
     </div>
   );
 };
